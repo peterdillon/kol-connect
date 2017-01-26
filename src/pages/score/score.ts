95eb8fa981d515d18a2181808c091ad7db9f9df2
Peter Dillon <peter.dillon@evolutionmedcom.com>
2017-01-26 11:19:55 -0500

    Added save and reset buttons, tweak display

--- src/pages/score/score.ts
+++ src/pages/score/score.ts
@@ -10,10 +10,14 @@ export class Score {
   constructor(public navCtrl: NavController) {}
 
 
-  criteria1: number = 40;
-  criteria2: number = 60;
-  criteria3: number = 20;
-  warmth: number = 1300;
+  criteria1: number = 5;
+  criteria2: number = 25;
+  criteria3: number = 15;
+  criteria4: number = 15;
+  criteria5: number = 15;
+  criteria6: number = 10;
+  criteria7: number = 80;
+  warmth: number = 15;
   // structure: any = {lower: 33, upper: 60};
 
   onChange(ev) {
